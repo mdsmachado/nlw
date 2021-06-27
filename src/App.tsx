8f003daeb2b9fbab549074d618399f5b07c24816
Marcelo Machado <spewar@gmail.com>
2021-06-27 15:20:34 -0300

    created 2 first windows on project and created auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import {Button} from "./componets/Button";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
   return (
-  <div>
-    <Button/>
-    <Button/>
-    <Button/>
-    <Button/>
-    <Button>clique aqui</Button>
-  </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/componets/Button.tsx
+++ src/componets/Button.tsx
@@ -1,9 +1,11 @@
-type ButtonProps = {
-  children?: string;
-}
+import {ButtonHTMLAttributes} from 'react'
 
-export function  Button(props: ButtonProps){
+import '../styles/button.scss'
+
+type ButtonProps=ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps){
   return(
-    <button>{props.children || 'Default'}</button>
+    <button className="button" {...props}/>
   )
 }
